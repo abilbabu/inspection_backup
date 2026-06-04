b79e03a262d6eb4f5c8de0c7f2b7529e685555fe
abilbabu <abilbabu2019@gmail.com>
2026-06-04 16:58:33 +0530

    app logo changes

--- lib/utils/constant/image_constants.dart
+++ lib/utils/constant/image_constants.dart
@@ -1,6 +1,6 @@
 class ImageConstants {
-   // ignore: constant_identifier_names
-   static const String Splachscreenlogo="assets/image/logo.png";
-   static const String loginlogo="assets/image/alm_logo_l.png";
-      static const String profileLogo="assets/image/default_profile.png";
+  // ignore: constant_identifier_names
+  static const String Splachscreenlogo = "assets/image/alm_logo_l.png";
+  static const String loginlogo = "assets/image/login.png";
+  static const String profileLogo = "assets/image/default_profile.png";
 }
